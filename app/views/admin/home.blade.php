97ac0615cfde011fdb08c28489941ae22ac40a9f
Chris Stroud <chris.stroud00@gmail.com>
2014-12-03 20:29:00 +0000

    Styled Admin

--- app/views/admin/home.blade.php
+++ app/views/admin/home.blade.php
@@ -5,38 +5,58 @@
 
 	<h4>Add Class</h4>
 	{{Form::open(array('route' => ['class.store'], 'method' =>'post'))}}
-	<div><input name="name"></div>
-	<button type="submit" class="btn btn-primary">Add</button>
+
+	<div class="row">
+		<div class="col-xs-12 col-sm-6">
+			<div class="table-element">
+				<input name="name" class="form-control">
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6">
+			<div class="table-element">
+				<button type="submit" class="btn btn-primary">Add</button>
+			</div>
+		</div>
+	</div>
+
 	{{Form::close()}}
 
 	<h2>Active Classes</h2>
 	@foreach ($classes as $class)
-	<div class="break"></div>
-	<div class="row">
-		<div class="col-sm-6 col-xs-12 row-text">{{ $class->name }}</div>
-		{{ Form::open(array('action' => ['ClassController@disable', $class->id], 'method' => 'post')) }}
-		<div class="col-sm-2 col-xs-12">
-			<button class="btn btn-warning">Disable</button>
+	<div class="table">
+		<div class="table-row">
+			<div class="col-sm-6 col-xs-12">
+				<div class="table-element">{{ $class->name }}</div>
+			</div>
+
+			{{ Form::open(array('action' => ['ClassController@disable', $class->id], 'method' => 'post')) }}
+			<div class="col-sm-2 col-xs-12">
+				<div class="table-element"><button class="btn btn-warning">Disable</button></div>
+			</div>
+			{{Form::close()}}
 		</div>
-		{{Form::close()}}
 	</div>
 	@endforeach
 
 	<h2>Disabled Classes</h2>
 	@foreach ($disabled as $class)
-	<div class="break"></div>
-	<div class="row">
-		<div class="col-sm-6 col-xs-12 row-text">{{ $class->name }}</div>
-		{{ Form::open(array('action' => ['ClassController@enable', $class->id], 'method' => 'post')) }}
-		<div class="col-sm-2 col-xs-6">
-			<button class="btn btn-success">Re-enable</button>
+	<div class="table">
+		<div class="table-row">
+			<div class="col-sm-6 col-xs-12">
+				{{ $class->name }}
+			</div>
+			{{ Form::open(array('action' => ['ClassController@enable', $class->id], 'method' => 'post')) }}
+			<div class="col-sm-2 col-xs-6">
+				<button class="btn btn-primary">Re-enable</button>
+			</div>
+			{{Form::close()}}
+			{{ Form::open(array('route' => ['class.destroy', $class->id], 'method' => 'delete')) }}
+			<div class="col-sm-2 col-xs-6">
+				<button class="btn btn-danger">Delete</button>
+			</div>
+			{{Form::close()}}
 		</div>
-		{{Form::close()}}
-		{{ Form::open(array('route' => ['class.destroy', $class->id], 'method' => 'delete')) }}
-		<div class="col-sm-2 col-xs-6">
-			<button class="btn btn-danger">Delete</button>
-		</div>
-		{{Form::close()}}
 	</div>
 	@endforeach
 
