86b41e7d230a803998d31132a8a3f98c081367ab
Chris Stroud <chris.stroud00@gmail.com>
2014-12-03 18:06:34 +0000

    Styled Login Page

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -12,7 +12,7 @@
 	{{ Form::open(array('action' => 'UserController@attemptLogin', 'id' => 'create-form')) }}
 
 	<div class="row">
-		<div class='col-sm-12 col-md-8'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('email', 'Email', ['class' => '']) }}
 				<input type="text" class="form-control" name="email" placeholder="example@mail.com">
@@ -21,20 +21,28 @@
 	</div>
 
 	<div class="row">
-		<div class='col-sm-12 col-md-8'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('password', 'Password', ['class' => '']) }}
 				<input type="password" class="form-control" name="password" placeholder="password">
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<button type="submit" class="btn btn-primary">Login</button>
+		</div>
+	</div>
 
-	<button type="submit" class="col-xs-12 col-md-6 btn btn-primary btn-submit">Login</button>
-
-
-	<div class="col-xs-12 col-md-7 alert alert-info alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-		Need an account? <a href="signup" class="alert-link">Click here</a> to create one!
+	<div class="col-xs-12">
+		<div class="row">
+			<div class="form-group">
+				<div class="alert alert-info alert-dismissible" role="alert">
+					<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					Need an account? <a href="signup" class="alert-link">Click here</a> to create one!
+				</div>
+			</div>
+		</div>
 	</div>
 
 
