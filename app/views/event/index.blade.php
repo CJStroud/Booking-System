0fcc6f29bd50872d2828d1156aa995b32e4954f0
Chris Stroud <chris.stroud00@gmail.com>
2014-12-03 19:02:53 +0000

    Styled home page

--- app/views/event/index.blade.php
+++ app/views/event/index.blade.php
@@ -5,39 +5,55 @@
 <div class="container">
 
 	<h2>Available to book</h2>
-	<hr>
-	@foreach($events as $event)
-	<div class="table-row row">
-		<div class="col-xs-12 col-sm-4 row-text">{{$event->name}}</div>
-		<div class="col-xs-12 col-sm-3 row-text">{{date('d/m/Y H:i', $event->event_datetime)}}</div>
-		<form action="event/{{$event->slug}}">
-			<div class="col-xs-12 col-sm-2">
-				<button class="btn btn-success btn-standard" type="submit">View</button>
-			</div>
-		</form>
-		<form action="booking/create/{{$event->slug}}">
-			<div class="col-xs-12 col-sm-2">
-				<button class="col-xs-4 col-sm-2 btn btn-primary btn-standard">Book</button>
-			</div>
-		</form>
-	</div>
-
-
-	@endforeach
-
-	<h2 class="second-header">Closed</h2>
-	<hr>
-	@foreach($old_events as $event)
-	<div class="table-row row">
-		<div class="col-xs-12 col-sm-4 row-text">{{$event->name}}</div>
-		<div class="col-xs-12 col-sm-3 row-text">{{date('d/m/Y H:i', $event->event_datetime)}}</div>
-		<form action="event/{{$event->slug}}">
-			<div class="col-xs-12 col-sm-2">
-				<button class=" btn btn-success btn-standard" type="submit">View</button>
-			</div>
-		</form>
-	</div>
-	@endforeach
-
 </div>
+	<div class="table">
+		@foreach($events as $event)
+		<div class="table-row">
+			<div class="container">
+			<div class="col-xs-12 col-sm-4">
+				<div class="table-element">{{$event->name}}</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-3">
+				<div class="table-element">{{date('d/m/Y H:i', $event->event_datetime)}}</div>
+			</div>
+			<form action="event/{{$event->slug}}">
+				<div class="col-xs-12 col-sm-2">
+					<button class="btn btn-primary" type="submit">View</button>
+				</div>
+			</form>
+			<form action="booking/create/{{$event->slug}}">
+				<div class="col-xs-12 col-sm-2">
+					<button class="btn btn-primary">Book</button>
+				</div>
+			</form>
+			</div>
+		</div>
+		@endforeach
+	</div>
+
+	<div class="container">
+		<h2>Closed</h2>
+	</div>
+	<div class="table">
+	@foreach($old_events as $event)
+	<div class="table-row">
+		<div class="container">
+			<div class="col-xs-12 col-sm-4">
+				<div class="table-element">{{$event->name}}</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-3">
+				<div class="table-element">{{date('d/m/Y H:i', $event->event_datetime)}}</div>
+			</div>
+			<form action="event/{{$event->slug}}">
+				<div class="col-xs-12 col-sm-2">
+					<button class="btn btn-primary" type="submit">View</button>
+				</div>
+			</form>
+		</div>
+	</div>
+	@endforeach
+	</div>
+
 
