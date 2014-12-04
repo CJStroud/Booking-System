33f149d891dd223794c7678a8b905f308c2c4374
Chris Stroud <chris.stroud00@gmail.com>
2014-12-04 00:26:13 +0000

    Styled Sign Up page

--- app/views/user/create.blade.php
+++ app/views/user/create.blade.php
@@ -4,13 +4,12 @@
 <div class="container">
 	<h1>Sign Up</h1>
 
-	<hr>
 	@include('partials.errors')
 
 	{{ Form::open(array('action' => 'UserController@store', 'id' => 'create-form')) }}
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('forename', 'Forename', ['class' => '']) }}
 				<input type="text" class="form-control" name="forename" value={{Input::old('forename')}}>
@@ -19,7 +18,7 @@
 	</div>
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('surname', 'Surname', ['class' => '']) }}
 				<input type="text" class="form-control" name="surname" value={{Input::old('surname')}}>
@@ -28,7 +27,7 @@
 	</div>
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('email', 'Email', ['class' => '']) }}
 				<input type="text" class="form-control" name="email" value={{Input::old('email')}}>
@@ -37,7 +36,7 @@
 	</div>
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('password', 'Password', ['class' => '']) }}
 				<input type="password" class="form-control" name="password" value={{Input::old('password')}}>
@@ -46,7 +45,7 @@
 	</div>
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('password_confirmation', 'Password Confirmation', ['class' => '']) }}
 				<input type="password" class="form-control" name="password_confirmation" value={{Input::old('password-confirmation')}}>
@@ -55,7 +54,7 @@
 	</div>
 
 	<div class="row">
-		<div class='col-xs-12 col-md-6'>
+		<div class='col-xs-12'>
 			<div class="form-group">
 				{{ Form::label('brca', 'BRCA Number', ['class' => '']) }}
 				<input type="text" class="form-control" name="brca" value={{Input::old('brca')}}>
@@ -63,13 +62,21 @@
 		</div>
 	</div>
 
-	<button type="submit" class="col-xs-12 col-md-6 btn-submit btn btn-primary">Create Account</button>
+	<div class="row">
+	<div class="col-xs-12">
+		<button type="submit" class="btn btn-primary">Create Account</button>
+	</div>
+	</div>
 
 	{{Form::close()}}
 
-	<div class="col-xs-12 col-md-7 alert alert-info alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-		Already have an account? <a href="login" class="alert-link">Login here</a>!
+	<div class="row">
+	<div class="col-xs-12">
+		<div class="alert alert-info alert-dismissible" role="alert">
+			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+			Already have an account? <a href="login" class="alert-link">Login here</a>!
+		</div>
+	</div>
 	</div>
 
 </div>
