b4bc06779386780cb112ef10021addb54673d5f4
Chris Stroud <chris.stroud00@gmail.com>
2014-12-05 20:21:31 +0000

    styled admin page

--- app/views/admin/home.blade.php
+++ app/views/admin/home.blade.php
@@ -24,38 +24,42 @@
 
 	<h2>Active Classes</h2>
 	@foreach ($classes as $class)
-	<div class="table">
-		<div class="table-row">
-			<div class="col-sm-6 col-xs-12">
-				<div class="table-element">{{ $class->name }}</div>
-			</div>
+	<div class="row">
+		<div class="table">
+			<div class="table-row">
+				<div class="col-sm-6 col-xs-12">
+					<div class="table-element">{{ $class->name }}</div>
+				</div>
 
-			{{ Form::open(array('action' => ['ClassController@disable', $class->id], 'method' => 'post')) }}
-			<div class="col-sm-2 col-xs-12">
-				<div class="table-element"><button class="btn btn-warning">Disable</button></div>
+				{{ Form::open(array('action' => ['ClassController@disable', $class->id], 'method' => 'post')) }}
+				<div class="col-sm-2 col-xs-12">
+					<div class="table-element"><button class="btn btn-warning">Disable</button></div>
+				</div>
+				{{Form::close()}}
 			</div>
-			{{Form::close()}}
 		</div>
 	</div>
 	@endforeach
 
 	<h2>Disabled Classes</h2>
 	@foreach ($disabled as $class)
-	<div class="table">
-		<div class="table-row">
-			<div class="col-sm-6 col-xs-12">
-				{{ $class->name }}
+	<div class="row">
+		<div class="table">
+			<div class="table-row">
+				<div class="col-sm-6 col-xs-12">
+					{{ $class->name }}
+				</div>
+				{{ Form::open(array('action' => ['ClassController@enable', $class->id], 'method' => 'post')) }}
+				<div class="col-sm-2 col-xs-6">
+					<button class="btn btn-primary">Re-enable</button>
+				</div>
+				{{Form::close()}}
+				{{ Form::open(array('route' => ['class.destroy', $class->id], 'method' => 'delete')) }}
+				<div class="col-sm-2 col-xs-6">
+					<button class="btn btn-danger">Delete</button>
+				</div>
+				{{Form::close()}}
 			</div>
-			{{ Form::open(array('action' => ['ClassController@enable', $class->id], 'method' => 'post')) }}
-			<div class="col-sm-2 col-xs-6">
-				<button class="btn btn-primary">Re-enable</button>
-			</div>
-			{{Form::close()}}
-			{{ Form::open(array('route' => ['class.destroy', $class->id], 'method' => 'delete')) }}
-			<div class="col-sm-2 col-xs-6">
-				<button class="btn btn-danger">Delete</button>
-			</div>
-			{{Form::close()}}
 		</div>
 	</div>
 	@endforeach
