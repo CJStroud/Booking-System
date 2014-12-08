2d5851823cfd02806f16c95439a32a273363635d
unknown <Chris.Stroud@D6HWJ12.GEOPOSTUKAD.COM>
2014-12-08 11:18:33 +0000

    Fixed booking seed

--- app/database/seeds/EventTableSeeder.php
+++ app/database/seeds/EventTableSeeder.php
@@ -12,10 +12,15 @@ class EventTableSeeder extends seeder {
         $hour = (60*60);
 
         $events = array (
-                    array('name' => 'Test Event 2',
-                            'slug' => 'test-event-2',
+                    array('name' => 'Test Event 1',
+                            'slug' => 'test-event-1',
                             'event_datetime' => strtotime('last friday') + ($hour * 19.5),
                             'close_datetime' => strtotime('last friday') + ($hour * 17)),
+
+                    array('name' => 'Test Event 2',
+                            'slug' => 'test-event-2',
+                            'event_datetime' => strtotime('next friday') + ($hour * 19.5),
+                            'close_datetime' => strtotime('next friday') + ($hour * 17))
         );
 
 
