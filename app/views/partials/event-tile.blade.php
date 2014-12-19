14dd639728c639e776ce5658f4f7c8a36655fc6f
Chris Normansell <chris.normansell@drpgroup.com>
2014-12-19 15:11:36 +0000

    Added booking close time and event start date to header

--- app/views/partials/event-tile.blade.php
+++ app/views/partials/event-tile.blade.php
@@ -2,17 +2,28 @@
 	<div class="tile">
 		<div class="tile-head">
 			<a href="#" class="event-fold">
-				<div class="col-xs-12">
+				<div class="col-xs-12 col-sm-6">
 					<h1><i class="fa fa-angle-double-up fa-spacing-right"></i>{{ $event->name }}</h1>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+					<h1 class="date">{{ date('dS F Y', $event->start_time) }}</h1>
 				</div>
 			</a>
 		</div>
 		<div class="tile-body">
 			<div class="tile-body-content">
 				<div class="col-xs-12 col-sm-6">
-					<p>Event Starts</p>
-					<div class="col-xs-12 time">
-						<p>{{ date('H:i', $event->start_time) }}</p>
+					<div class="col-xs-12 col-sm-6">
+						<p>Booking Closes</p>
+						<div class="time">
+							<p>{{ date('H:i', $event->close_time) }}</p>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6">
+						<p>Event Starts</p>
+						<div class="time">
+							<p>{{ date('H:i', $event->start_time) }}</p>
+						</div>
 					</div>
 				</div>
 				<div class="col-xs-12 col-sm-6">
