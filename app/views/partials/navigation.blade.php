6d06943b977b80bfb5bba83f850b77a0ec25b256
Piefarmer <chris.normansell@gmail.com>
2014-12-30 16:40:54 +0000

    Update nav to have new link to user profile

--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -14,7 +14,7 @@
       <div class="collapse navbar-collapse" id="main-navbar-collapsable">
         <ul class="nav navbar-nav">
           <li><a href="{{URL::route('event.index')}}"><i class="fa fa-users fa-lg fa-spacing-right"></i>Events</a></li>
-          <li><a href="{{URL::action('BookingController@viewAll')}}"><i class="fa fa-calendar fa-lg"></i> My Bookings</a></li>
+          <li><a href="{{URL::action('BookingController@viewAll')}}"><i class="fa fa-calendar fa-lg fa-spacing-right"></i>My Bookings</a></li>
 
           @if (Auth::check() && Auth::user()->is_admin)
           <li><a href="{{URL::action('HomeController@AdminHome')}}"><i class="fa fa-gears fa-lg fa-spacing-right"></i>Admin</a></li>
@@ -23,10 +23,11 @@
         <ul class="nav navbar-nav navbar-right">
           <li class="nav-divider"></li>
           @if (Auth::check())
-            <li>
-              <p class="navbar-text">Logged in as {{ Auth::user()->forename . ' ' .Auth::user()->surname }}</p></li>
-          <li><a href="{{URL::action('UserController@signOut')}}" class="navbar-link">Sign Out<i class="fa fa-sign-out fa-lg fa-spacing-left"></i></a>
-            </li>
+          <li><a href="{{URL::action('UserController@login')}}">
+            <i class="svg-icon svg-user fa-spacing-right"></i>{{ Auth::user()->forename }}
+          </a></li>
+            <li><a href="{{URL::action('UserController@signOut')}}" class="navbar-link">Sign Out<i class="fa fa-sign-out fa-lg fa-spacing-left"></i></a>
+              </li>
           @else
             <li><a href="{{URL::action('UserController@login')}}"><i class="fa fa-sign-in fa-lg fa-spacing-right"></i>Login</a></li>
 
