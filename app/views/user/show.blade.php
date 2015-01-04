a0584185f1778242eb93e1350bb9b7de74b85a32
Piefarmer <chris.normansell@gmail.com>
2015-01-04 17:54:20 +0000

    Styled profile image and h1

--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <div class="bg-lightgray">
-  <div class="container">
+  <div class="container profile">
 
     @include('partials.errors')
     <div class="row">
-      <div class="col-xs-6 col-sm-4">
+      <div class="col-xs-6 col-sm-3">
         <img src="/img/profile-image.svg" alt="profile image" class="profile-image img-circle">
 
       </div>
-      <div class="col-xs-6 col-sm-4">
-        <h3>{{ $user->forename }} {{ $user->surname }}</h3>
+      <div class="col-xs-6 col-sm-9">
+        <h1>{{ $user->forename }} {{ $user->surname }}</h1>
       </div>
   </div>
 </div>
