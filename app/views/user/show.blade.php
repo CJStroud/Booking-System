fc7cfa8500b4cec8869e15dcec08ab9cc64be093
Piefarmer <chris.normansell@gmail.com>
2015-01-04 17:39:39 +0000

    Start creating user profile managment

--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -1,38 +1,17 @@
 @include('layouts.master')
 
 @section('content')
-<div class="container">
-  <h1>{{ $user->forename }} {{ $user->surname }}</h1>
+<div class="bg-lightgray">
+  <div class="container">
 
-  @include('partials.errors')
-  <div class="row">
+    @include('partials.errors')
+    <div class="row">
+      <div class="col-xs-6 col-sm-4">
+        <img src="/img/profile-image.svg" alt="profile image" class="profile-image img-circle">
 
-  </div>
-  {{ Form::open(array('action' => 'UserController@attemptLogin', 'id' => 'create-form')) }}
-
-  <div class="row">
-    <div class='col-xs-12'>
-      <div class="form-group">
-        {{ Form::label('email', 'Email', ['class' => '']) }}
-        <input type="text" class="form-control" name="email" placeholder="example@mail.com">
       </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class='col-xs-12'>
-      <div class="form-group">
-        {{ Form::label('password', 'Password', ['class' => '']) }}
-        <input type="password" class="form-control" name="password" placeholder="password">
+      <div class="col-xs-6 col-sm-4">
+        <h3>{{ $user->forename }} {{ $user->surname }}</h3>
       </div>
-    </div>
   </div>
-  <div class="row">
-    <div class="col-xs-12 col-sm-2 col-sm-offset-10">
-      <button type="submit" class="btn btn-primary btn-with-addon"><span class="btn-text">Login</span><span class="btn-addon btn-addon-primary"><i class="fa fa-arrow-right"></i></span></button>
-    </div>
-  </div>
-
-  {{ Form::close() }}
-
 </div>
