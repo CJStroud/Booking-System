26afbb1c648a9db57027c29ac28b4bdc14db5659
unknown <Chris.Stroud@D6HWJ12.GEOPOSTUKAD.COM>
2015-01-05 11:57:52 +0000

    added contact us page

---
+++ app/views/info/contact.blade.php
@@ -0,0 +1,52 @@
+@include('layouts.master')
+
+@section('content')
+<div class="container">
+    <h1>Contact Us</h1>
+
+    <div class="list-group">
+        <div class="list-group-item list-group-item-info">
+            <h4 class="list-group-item-heading">Club Committee</h4>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Chairman and Webmaster</h4>
+            <p class="list-group-item-text">Andrew Bird</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Vice Chairman</h4>
+            <p class="list-group-item-text">Trevor Stanley</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Treasurer</h4>
+            <p class="list-group-item-text">Alan Meredith</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Secretary</h4>
+            <p class="list-group-item-text">Sarah Meredith</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Club BRCA Rep</h4>
+            <p class="list-group-item-text">Alan Meredith</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Acting Race Control</h4>
+            <p class="list-group-item-text">Jamie Beasley</p>
+        </div>
+
+        <div class="list-group-item">
+            <h4 class="list-group-item-heading">Committee Memebers</h4>
+            <p class="list-group-item-text">Ade Malkin</p>
+            <p class="list-group-item-text">Mark Beckett</p>
+            <p class="list-group-item-text">Jonathan Willey</p>
+            <p class="list-group-item-text">Kirk Beasley</p>
+        </div>
+
+    </div>
+
+</div>
