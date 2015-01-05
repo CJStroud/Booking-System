9e7f4257ddda9eb6210b229d9026617bc59b4959
unknown <Chris.Stroud@D6HWJ12.GEOPOSTUKAD.COM>
2015-01-05 10:07:50 +0000

    created about us page

---
+++ app/views/info/about.blade.php
@@ -0,0 +1,24 @@
+@include('layouts.master')
+
+@section('content')
+<div class="container">
+<h1>About Us</h1>
+
+    <div class="col-xs-12">
+        <p>
+            We are a friendly club and race every friday at Hadden Hill Leisure Centre 18.30-22:30pm. Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.
+        </p>
+        <p>Doors open at 18:30, we have lots of tables, chairs and power points available!</p>
+        <p>any help we get putting the track out will get you racing quicker!!</p>
+    </div>
+
+    <h3>Address</h3>
+    <div class="col-xs-12">
+        <div class="row">Haden Hill Leisure Centre</div>
+        <div class="row">Barrs Road</div>
+        <div class="row">Cradley Heath</div>
+        <div class="row">B64 7HA</div>
+    </div>
+
+
+</div>
