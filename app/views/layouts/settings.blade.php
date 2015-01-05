1e8e83eb14b4dd806a03784818575dda093d75fc
Piefarmer <chris.normansell@gmail.com>
2015-01-05 21:20:51 +0000

    Added content section and swapping active state

--- app/views/layouts/settings.blade.php
+++ app/views/layouts/settings.blade.php
@@ -5,17 +5,28 @@
   <div class="container settings">
     <h2>Settings</h2>
     @include('partials.errors')
-
-    <div class="list-group col-xs-1 col-sm-3 col-md-2">
-      <a href="{{ route('settings.profile') }}" class="list-group-item active">
+    <div class="list-group col-xs-2 col-sm-3 col-md-2">
+      @if ($active == 'profile')
+        <a href="{{ route('settings.profile') }}" class="list-group-item active">
+      @else
+        <a href="{{ route('settings.profile') }}" class="list-group-item">
+      @endif
         <i class="fa fa-newspaper-o fa-lg"></i><span class="hidden-xs icon-spacing-left">Profile</span>
       </a>
 
-      <a href="{{ route('settings.profile') }}" class="list-group-item">
+      @if ($active == 'account')
+        <a href="{{ route('settings.profile') }}" class="list-group-item active">
+      @else
+        <a href="{{ route('settings.profile') }}" class="list-group-item">
+      @endif
         <i class="fa fa-user fa-lg"></i><span class="hidden-xs icon-spacing-left">Account</span>
       </a>
     </div>
+    <div class="col-xs-10 col-sm-9 col-md-10">
+      @yield('settings-content')
+    </div>
   </div>
+
 
 </div>
 
