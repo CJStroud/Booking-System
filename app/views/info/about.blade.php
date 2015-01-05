91eeb9f56824696f64958787254ac9598b31b95b
unknown <Chris.Stroud@D6HWJ12.GEOPOSTUKAD.COM>
2015-01-05 11:24:26 +0000

    reformated the text on the about us  page

--- app/views/info/about.blade.php
+++ app/views/info/about.blade.php
@@ -3,14 +3,11 @@
 @section('content')
 <div class="container">
 <h1>About Us</h1>
-
-    <div class="col-xs-12">
-        <p>
-            We are a friendly club and race every friday at Haden Hill Leisure Centre 18.30-22:30pm. Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.
+        <p>We are a friendly club and race every friday at Haden Hill Leisure Centre 18.30-22:30pm.</p>
+        <p>Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.
         </p>
         <p>Doors open at 18:30, we have lots of tables, chairs and power points available!</p>
-        <p>any help we get putting the track out will get you racing quicker!!</p>
-    </div>
+        <p>Any help we get putting the track out will get you racing quicker!! <i class="fa fa-flag-checkered"></i></p>
 
     <h3>Address</h3>
     <div class="col-xs-12">
