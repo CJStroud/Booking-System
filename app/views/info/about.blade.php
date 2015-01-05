178ebaad3245b0ffffb39c1495e6fb47ecd0959f
unknown <Chris.Stroud@D6HWJ12.GEOPOSTUKAD.COM>
2015-01-05 11:08:40 +0000

    Fixed mis-spelling of haden

--- app/views/info/about.blade.php
+++ app/views/info/about.blade.php
@@ -6,7 +6,7 @@
 
     <div class="col-xs-12">
         <p>
-            We are a friendly club and race every friday at Hadden Hill Leisure Centre 18.30-22:30pm. Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.
+            We are a friendly club and race every friday at Haden Hill Leisure Centre 18.30-22:30pm. Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.
         </p>
         <p>Doors open at 18:30, we have lots of tables, chairs and power points available!</p>
         <p>any help we get putting the track out will get you racing quicker!!</p>
