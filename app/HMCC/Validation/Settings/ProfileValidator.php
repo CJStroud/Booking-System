2c3801840c67524b6a7f61b204b9b9f67ddffad2
Piefarmer <chris.normansell@gmail.com>
2015-01-05 22:33:10 +0000

    Added skill validation

--- app/HMCC/Validation/Settings/ProfileValidator.php
+++ app/HMCC/Validation/Settings/ProfileValidator.php
@@ -5,7 +5,8 @@ use HMCC\Validation\Validator;
 class ProfileValidator extends Validator
 {
   protected $rules = ['forename' => 'sometimes|required',
-                      'surname' => 'sometimes|required',
-                      'email' => 'sometimes|required|email|unique:users',
-                      'brca' => 'sometimes|required|numeric'];
+                      'surname'  => 'sometimes|required',
+                      'email'    => 'sometimes|required|email|unique:users',
+                      'brca'     => 'sometimes|required|numeric',
+                      'skill'    => 'sometimes|required|numeric|between:0,10'];
 }
