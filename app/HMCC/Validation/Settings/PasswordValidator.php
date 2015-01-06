92011c8d686a9a2863fd107796902d49794137e3
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 10:10:12 +0000

    Added password validator

---
+++ app/HMCC/Validation/Settings/PasswordValidator.php
@@ -0,0 +1,22 @@
+<?php namespace HMCC\Validation\Settings;
+
+use HMCC\Validation\Validator;
+use Hash;
+
+class PasswordValidator extends Validator
+{
+  protected $rules = ['new-password' => 'required|min:6|confirmed'];
+
+  public function passes(Array $inputs)
+  {
+    $passed = parent::passes($inputs);
+    $newPassword = $inputs['old-password'] . Auth::user()->secret;
+
+    if (!Hash::check($newPassword, Auth::user()->password))
+    {
+      $this->errors->add('old-password', 'Old password is incorrect');
+      $passed = false;
+    }
+    return $passed;
+  }
+}
