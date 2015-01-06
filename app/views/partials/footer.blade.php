edca0d9eccd470785963cbc21ef9e87a25a446fa
Chris Stroud <chris.stroud00@gmail.com>
2015-01-06 12:57:51 +0000

    professionalised footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,25 +1,26 @@
-<div class="footer">
-    <div class="container">
+<div class="footer-wrapper">
 
-        <ul class="footer-links">
-            <li><a href='#'>contact us</a></li>
-            <li><a href='#'>about us</a></li>
-            <li><a href='#'>gallery</a></li>
-            <li><a href='#'>events</a></li>
+    <div class="footer">
+        <div class="container">
 
-            @if (Auth::check())
-            <li><a href='#'>sign out</a></li>
-            <li><a href='#'>my profile</a></li>
-            @else
-            <li><a href='#'>login</a></li>
-            <li><a href='#'>sign up</a></li>
-            @endif
-        </ul>
+            <ul class="footer-links">
+                <li><a href='#'>contact us</a></li>
+                <li><a href='#'>about us</a></li>
+                <li><a href='#'>gallery</a></li>
+                <li><a href='#'>events</a></li>
+
+                @if (Auth::check())
+                <li><a href='#'>sign out</a></li>
+                <li><a href='#'>my profile</a></li>
+                @else
+                <li><a href='#'>login</a></li>
+                <li><a href='#'>sign up</a></li>
+                @endif
+            </ul>
+        </div>
+    </div>
+
+    <div class="footer-copyright">
+        <span><i class="fa fa-copyright"></i> 2015 Halesowen Model Car Club</span><span class="author">Created by Chris Stroud and Chris Normansell</span>
     </div>
 </div>
-
-
-<div class="footer-copyright">
-    <p><i class="fa fa-copyright"></i> 2015 Halesowen Model Car Club | Created by Chris Stroud and Chris Normansell</p>
-</div>
-
