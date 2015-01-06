dfaab03cf65b66c798a9ad11738c35057fc2eea6
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 15:34:46 +0000

    Removed old admin crap

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -4,25 +4,6 @@ class AdminController extends BaseController {
 
   public function home()
   {
-    // check user is admin
-    $user = Session::get('user');
-    if ($user == null || !$user->isAdmin)
-    {
-      //if not admin direct back to home/events
-      return Redirect::route('event.index');
-    }
-    else
-    {
-      // get active classes
-      $classes = DB::select('SELECT * FROM class WHERE active = true');
-
-      // get disabled classes
-      $disabled = DB::select('SELECT * FROM class WHERE active = false');
-
-      // create admin page and pass in active and disabled classes
-      return View::make('admin.home')->withClasses($classes)->withDisabled($disabled);
-    }
-
+      return View::make('admin.home');
   }
-
 }
--- app/views/admin/home.blade.php
+++ app/views/admin/home.blade.php
@@ -2,68 +2,7 @@
 
 @section('content')
 <div class="container">
-
-    <h4>Add Class</h4>
-    {{Form::open(array('route' => ['class.store'], 'method' =>'post'))}}
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-6">
-            <div class="table-element">
-                <input name="name" class="form-control">
-            </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-6">
-            <div class="table-element">
-                <button type="submit" class="btn btn-primary">Add</button>
-            </div>
-        </div>
-    </div>
-
-    {{Form::close()}}
-
-    <h2>Active Classes</h2>
-    @foreach ($classes as $class)
-    <div class="row">
-        <div class="table">
-            <div class="table-row">
-                <div class="col-sm-6 col-xs-12">
-                    <div class="table-element">{{ $class->name }}</div>
-                </div>
-
-                {{ Form::open(array('route' => ['class.disable', $class->id], 'method' => 'post')) }}
-                <div class="col-sm-2 col-xs-12">
-                    <div class="table-element"><button class="btn btn-warning">Disable</button></div>
-                </div>
-                {{Form::close()}}
-            </div>
-        </div>
-    </div>
-    @endforeach
-
-    <h2>Disabled Classes</h2>
-    @foreach ($disabled as $class)
-    <div class="row">
-        <div class="table">
-            <div class="table-row">
-                <div class="col-sm-6 col-xs-12">
-                    {{ $class->name }}
-                </div>
-                {{ Form::open(array('route' => ['class.enable', $class->id], 'method' => 'post')) }}
-                <div class="col-sm-2 col-xs-6">
-                    <button class="btn btn-primary">Re-enable</button>
-                </div>
-                {{Form::close()}}
-                {{ Form::open(array('route' => ['class.destroy', $class->id], 'method' => 'delete')) }}
-                <div class="col-sm-2 col-xs-6">
-                    <button class="btn btn-danger">Delete</button>
-                </div>
-                {{Form::close()}}
-            </div>
-        </div>
-    </div>
-    @endforeach
-
+  <h1>Admin page</h1>
+  <h2>When a developer can be arsed</h2>
 </div>
-
 @stop
