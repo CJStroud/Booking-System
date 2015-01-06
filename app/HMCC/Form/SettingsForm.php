4e4c3011a0a235917348b1145c321eb203a376c0
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 10:14:57 +0000

    Added password update to the settings form

--- app/HMCC/Form/SettingsForm.php
+++ app/HMCC/Form/SettingsForm.php
@@ -6,14 +6,33 @@ use Auth;
 
 class SettingsForm extends Form
 {
-  public function __construct(UserRepository $repository, ProfileValidator $validator)
+  /**
+   *  @var PasswordValidator
+   */
+  protected $passwordValidator;
+
+  /**
+   * @var ProfileValidator
+   */
+
+  protected $profileValidator;
+
+  public function __construct(UserRepository $repository, ProfileValidator $profileValidator, PasswordValidator $passwordValidator)
   {
     $this->repository = $repository;
-    $this->validator = $validator;
+    $this->validator = $profileValidator;
+    $this->profileValidator = $profileValidator;
+    $this->passwordValidator = $passwordValidator;
   }
 
+  /**
+   * This will update the user record
+   * @param Array $data An array of all the inputs from the form.
+   */
   public function profileUpdate($data)
   {
+    $this->validator = $this->profileValidator;
+
     $user = Auth::user();
 
     $inputs = array_keys($data);
@@ -30,5 +49,19 @@ class SettingsForm extends Form
     }
 
     parent::update(Auth::id(), $data);
+  }
+
+  /**
+   * Validates and updates the password for the user
+   * @param   Array    $data An array of all the inputs for the password update. old-password, new-password, new-password-confirmation
+   * @returns [[Type]] [[Description]]
+   */
+  public function passwordUpdate($data)
+  {
+    $this->validator = $this->passwordValidator;
+
+    $this->validate($data);
+
+    return $this->repository->passwordUpdate($data);
   }
 }
