e75c7b7317a2b4da6d9777306e6f3d0b7c56341a
Chris Stroud <chris.stroud00@gmail.com>
2015-01-06 21:53:49 +0000

    renamed lock / unlock function variables

--- app/controllers/RaceEventController.php
+++ app/controllers/RaceEventController.php
@@ -7,143 +7,143 @@ use HMCC\Repository\Booking\BookingRepository;
 
 class RaceEventController extends \BaseController {
 
-    /**
-     * @var RaceEventForm
-     */
-    protected $form;
+  /**
+   * @var RaceEventForm
+   */
+  protected $form;
 
-    /**
-     * @var RaceEventClassRepository
-     */
-    protected $raceEventClassRepository;
+  /**
+   * @var RaceEventClassRepository
+   */
+  protected $raceEventClassRepository;
 
-    /**
-     * @var RaceClassRepository
-     */
-    protected $raceClassRepository;
+  /**
+   * @var RaceClassRepository
+   */
+  protected $raceClassRepository;
 
-    /**
-     * @var BookingRepository
-     */
-    protected $bookingRepository;
+  /**
+   * @var BookingRepository
+   */
+  protected $bookingRepository;
 
-    public function __construct(
-        RaceEventForm $form,
-        RaceEventClassRepository $raceEventClassRepository,
-        RaceClassRepository $raceClassRepository,
-        BookingRepository $bookingRepository)
+  public function __construct(
+    RaceEventForm $form,
+    RaceEventClassRepository $raceEventClassRepository,
+    RaceClassRepository $raceClassRepository,
+    BookingRepository $bookingRepository)
+  {
+    $this->form = $form;
+    $this->raceClassRepository = $raceClassRepository;
+    $this->raceEventClassRepository = $raceEventClassRepository;
+    $this->bookingRepository = $bookingRepository;
+  }
+
+  /**
+   * Shows all of the events
+   * @returns Event.Index View
+   */
+  public function index()
+  {
+    $timestamp = time();
+
+    $allEvents = $this->form->repository->getAllInDateOrder();
+
+    $events = [];
+
+    $now = time();
+
+    foreach ($allEvents as $event)
     {
-        $this->form = $form;
-        $this->raceClassRepository = $raceClassRepository;
-        $this->raceEventClassRepository = $raceEventClassRepository;
-        $this->bookingRepository = $bookingRepository;
+      if ($event->start_time <= $now)
+      {
+        $event->isFinished = true;
+      }
+      else if ($event->close_time <= $now)
+      {
+        $event->isClosed = true;
+      }
+
+      $events[] = $event;
+    }
+    return View::make('event.index')->with('events', $events);
+  }
+
+
+  /**
+   * Creates the view for a new event
+   * @returns Event.Create View
+   */
+  public function create()
+  {
+    $classes = $this->raceClassRepository->getAllActive();
+
+    return View::make('event.create')->withOptions($classes);
+  }
+
+  /**
+   * Stores a new event using form inputs
+   * @returns Event.Index View
+   */
+  public function store()
+  {
+    $this->form->store(Input::all());
+
+    return Redirect::route('event.index');
+  }
+
+  /**
+   * Gets event information using slug
+   * @param integer $slug The   slug of the event
+   * @returns Event.View View
+   */
+  public function show($slug)
+  {
+    $event = $this->form->repository->getEventBySlug($slug);
+    $classes = $event->classes;
+
+    foreach ($classes as $class)
+    {
+      $class->bookings = $this->bookingRepository->getBookingsByEventIdAndClassId($event->id, $class->id);
+
     }
 
-    /**
-     * Shows all of the events
-     * @returns Event.Index View
-     */
-    public function index()
-    {
-        $timestamp = time();
+    return View::make('event.view')->with('classes', $classes)->with('event', $event);
+  }
 
-        $allEvents = $this->form->repository->getAllInDateOrder();
+  /**
+   * Deletes the event using the id
+   * @param integer $id The id of the evnt to delete
+   */
+  public function destroy($id)
+  {
+    $event = RaceEvent::find($id);
+    $event->delete();
+  }
 
-        $events = [];
+  /**
+   * Locks the event class
+   * @param   integer $eventId The id of the event
+   * @param   integer $classId The id of the class
+   * @returns Laravel redirect back
+   */
+  public function lock($event_id, $class_id)
+  {
+    $this->eventClassRepository->lock($event_id, $class_id);
 
-        $now = time();
+    return Redirect::back();
+  }
 
-        foreach ($allEvents as $event)
-        {
-            if ($event->start_time <= $now)
-            {
-                $event->isFinished = true;
-            }
-            else if ($event->close_time <= $now)
-            {
-                $event->isClosed = true;
-            }
+  /**
+   * Unlocks the event class
+   * @param   integer $eventId The id of the event
+   * @param   integer $classId The id of the class
+   * @returns Laravel redirect back
+   */
+  public function unlock($event_id, $class_id)
+  {
+    $this->eventClassRepository->unlock($event_id, $class_id);
 
-            $events[] = $event;
-        }
-        return View::make('event.index')->with('events', $events);
-    }
-
-
-    /**
-     * Creates the view for a new event
-     * @returns Event.Create View
-     */
-    public function create()
-    {
-        $classes = $this->raceClassRepository->getAllActive();
-
-        return View::make('event.create')->withOptions($classes);
-    }
-
-    /**
-     * Stores a new event using form inputs
-     * @returns Event.Index View
-     */
-    public function store()
-    {
-        $this->form->store(Input::all());
-
-        return Redirect::route('event.index');
-    }
-
-    /**
-     * Gets event information using slug
-     * @param integer $slug The   slug of the event
-     * @returns Event.View View
-     */
-    public function show($slug)
-    {
-        $event = $this->form->repository->getEventBySlug($slug);
-        $classes = $event->classes;
-
-        foreach ($classes as $class)
-        {
-            $class->bookings = $this->bookingRepository->getBookingsByEventIdAndClassId($event->id, $class->id);
-
-        }
-
-        return View::make('event.view')->with('classes', $classes)->with('event', $event);
-    }
-
-    /**
-     * Deletes the event using the id
-     * @param integer $id The id of the evnt to delete
-     */
-    public function destroy($id)
-    {
-        $event = RaceEvent::find($id);
-        $event->delete();
-    }
-
-    /**
-     * Locks the event class
-     * @param   integer  $classId The id of the class
-     * @param   integer  $eventId The id of the event
-     * @returns Laravel redirect back
-     */
-    public function lock($classId, $eventId)
-    {
-        $this->eventClassRepository->lock($eventId, $classId);
-
-        return Redirect::back();
-    }
-
-    /**
-     * Unlocks the event class
-     * @param   integer  $classId The id of the class
-     * @param   integer  $eventId The id of the event
-     * @returns Laravel redirect back
-     */
-    public function unlock($classId, $eventId)
-    {
-        $this->eventClassRepository->unlock($eventId, $classId);
-
-        return Redirect::back();
-    }
+    return Redirect::back();
+  }
 }
