db3536c0d7bed4f9911774523fef31acd3046ac8
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 12:07:22 +0000

    Sorted out buttons on modal

--- app/views/settings/account.blade.php
+++ app/views/settings/account.blade.php
@@ -44,6 +44,9 @@
   <div class="panel-heading">Delete account</div>
   <div class="panel-body">
     <div class="form-group">
+      <div class="col-xs-12">
+        <p>Once the account is deleted, it is gone. There is no going back. Please make sure this is what you want to do.</p>
+      </div>
       <div class="col-xs-12 col-sm-4 col-md-2">
         <button type="submit" class="btn btn-primary btn-with-addon" data-toggle="modal" data-target="#deleteAccount"><span class="btn-text">Delete</span><span class="btn-addon btn-addon-delete"><i class="fa fa-trash"></i></span></button>
       </div>
@@ -54,24 +57,26 @@
 <div class="modal fade modal-delete-account" id="deleteAccount" tabindex="-1" role="dialog" aria-labelledby="Delete account" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Delete Account</h4>
-      </div>
-      <div class="modal-body">
-        {{ Form::open(['route' => ['settings.account.delete'], 'role' => 'form', 'id' => 'form', 'method' => 'POST' ] ) }}
-          <div class="form-group">
-            <div class="col-xs-12 col-sm-4 col-md-2">
-              <button type="submit" class="btn btn-delete">Delete</button>
-            </div>
-          </div>
-        {{ Form::close() }}
-      </div>
-      <div class="modal-footer">
-        <div class="col-xs-6 col-sm-3 col-sm-offset-9">
-          <button type="button" class="btn btn-default btn-with-addon" data-dismiss="modal"><span class="btn-text">Close</span><span class="btn-addon btn-addon-primary"><i class="fa fa-close"></i></span></button>
+      {{ Form::open(['route' => ['settings.account.delete'], 'role' => 'form', 'id' => 'form', 'method' => 'POST' ] ) }}
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">Delete Account</h4>
         </div>
-      </div>
+        <div class="modal-body">
+            <div class="form-group">
+              <div class="col-xs-12 col-sm-4 col-md-2">
+              </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <div class="col-xs-6 col-sm-3 col-sm-offset-6">
+              <button type="submit" class="btn btn-delete">Delete</button>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <button type="button" class="btn btn-default btn-with-addon" data-dismiss="modal"><span class="btn-text">Close</span><span class="btn-addon btn-addon-primary"><i class="fa fa-close"></i></span></button>
+          </div>
+        </div>
+      {{ Form::close() }}
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
