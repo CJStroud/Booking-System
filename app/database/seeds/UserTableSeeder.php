720e705a34e2a1daa2904325bd483583654e50f1
Chris Stroud <chris.stroud00@gmail.com>
2015-01-06 22:51:25 +0000

    update reference to user repo in user table seeder

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,48 +1,48 @@
 <?php
 
-use HMCC\Repository\UserRepository;
+use HMCC\Repository\User\UserRepository;
 
 class UserTableSeeder extends seeder {
 
-	protected $repository;
+  protected $repository;
 
-	public function __construct(UserRepository $repository)
-	{
-		$this->repository = $repository;
-	}
+  public function __construct(UserRepository $repository)
+  {
+    $this->repository = $repository;
+  }
 
-	public function run()
-	{
-		DB::table('users')->delete();
+  public function run()
+  {
+    DB::table('users')->delete();
 
-		$secret = str_random(15);
-		$user = array(
-			'forename' => 'Chris',
-			'surname' => 'Stroud',
-			'email' => 'chris.stroud@gmail.com',
-			'password' => 'password',
-			'secret' => $secret,
-			'brca' => '213048',
-			'transponder' => '1273823',
-			'skill' => 5,
-			'is_admin' => true
-		);
-		$this->repository->store($user);
+    $secret = str_random(15);
+    $user = array(
+      'forename' => 'Chris',
+      'surname' => 'Stroud',
+      'email' => 'chris.stroud@gmail.com',
+      'password' => 'password',
+      'secret' => $secret,
+      'brca' => '213048',
+      'transponder' => '1273823',
+      'skill' => 5,
+      'is_admin' => true
+    );
+    $this->repository->store($user);
 
-		$secret = str_random(15);
-		$user = array(
-			'forename' => 'Andy',
-			'surname' => 'Bird',
-			'email' => 'andy.bird@gmail.com',
-			'password' => Hash::make('password' . $secret),
-			'secret' => $secret,
-			'brca' => '9382372',
-			'transponder' => '93047563',
-			'skill' => 8,
-			'is_admin' => false,
-		);
-		$this->repository->store($user);
+    $secret = str_random(15);
+    $user = array(
+      'forename' => 'Andy',
+      'surname' => 'Bird',
+      'email' => 'andy.bird@gmail.com',
+      'password' => Hash::make('password' . $secret),
+      'secret' => $secret,
+      'brca' => '9382372',
+      'transponder' => '93047563',
+      'skill' => 8,
+      'is_admin' => false,
+    );
+    $this->repository->store($user);
 
-	}
+  }
 
 }
