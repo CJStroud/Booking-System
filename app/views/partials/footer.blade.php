c1cbb51a2a25cb04305992e1eccebd56d872a853
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 14:37:08 +0000

    Added links to footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -4,17 +4,17 @@
         <div class="container">
 
             <ul class="footer-links">
-                <li><a href='#'>contact us</a></li>
-                <li><a href='#'>about us</a></li>
-                <li><a href='#'>gallery</a></li>
-                <li><a href='#'>events</a></li>
+                <li><a href="{{ route('contact') }}">contact us</a></li>
+                <li><a href="{{ route('about') }}">about us</a></li>
+                <li><a href="{{ route('gallery') }}">gallery</a></li>
+                <li><a href="{{ route('event.index') }}">events</a></li>
 
                 @if (Auth::check())
-                <li><a href='#'>sign out</a></li>
-                <li><a href='#'>my profile</a></li>
+                  <li><a href="{{ route('settings.profile') }}">my profile</a></li>
+                  <li><a href="{{ route('user.logout') }}">log out</a></li>
                 @else
-                <li><a href='#'>login</a></li>
-                <li><a href='#'>sign up</a></li>
+                  <li><a href="{{ route('user.login') }}">login</a></li>
+                  <li><a href="{{ route('user.signup') }}">sign up</a></li>
                 @endif
             </ul>
         </div>
