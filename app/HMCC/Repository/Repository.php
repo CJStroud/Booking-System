2fd4f205a44c33c8383e6fb8bb4106102e3469de
Chris Normansell <chris.normansell@drpgroup.com>
2015-01-06 13:01:32 +0000

    Fixed delete function in repository

--- app/HMCC/Repository/Repository.php
+++ app/HMCC/Repository/Repository.php
@@ -4,48 +4,49 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class Repository implements RepositoryInterface
 {
-	protected $model;
+  protected $model;
 
-	public function __construct(Model $model)
-	{
-		$this->model = $model;
-	}
+  public function __construct(Model $model)
+  {
+    $this->model = $model;
+  }
 
-	// TODO add comments
-	public function all()
-	{
-		return $this->model->all();
-	}
+  // TODO add comments
+  public function all()
+  {
+    return $this->model->all();
+  }
 
-	public function store($data)
-	{
-		$newModel = new $this->model;
+  public function store($data)
+  {
+    $newModel = new $this->model;
 
-		$newModel->fill($data);
+    $newModel->fill($data);
 
-		return $newModel->save();
-	}
+    return $newModel->save();
+  }
 
-	public function delete($id)
-	{
-		return $this->destroy($id);
-	}
+  public function delete($id)
+  {
+    $record = $this->find($id);
+    return $record->destroy($id);
+  }
 
-	public function update($id, $data)
-	{
-		$record = $this->find($id);
+  public function update($id, $data)
+  {
+    $record = $this->find($id);
 
-		$record->fill($data);
+    $record->fill($data);
 
-		return $record->save();
-	}
+    return $record->save();
+  }
 
-	public function find($id)
-	{
-		// TODO check what fail does
+  public function find($id)
+  {
+    // TODO check what fail does
 
-		return $this->model->findOrFail($id);
-	}
+    return $this->model->findOrFail($id);
+  }
 
 
 }
