aadf7dfe8f8ba0c1bf941948148e321a9f78e383
Chris Stroud <chris.stroud00@gmail.com>
2015-01-06 22:48:41 +0000

    added primary key to event class table

--- app/database/migrations/2014_11_23_111700_create_event_class_table.php
+++ app/database/migrations/2014_11_23_111700_create_event_class_table.php
@@ -5,37 +5,39 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateEventClassTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create( 'event_classes', function( $table ) {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create( 'event_classes', function( $table ) {
 
-			$table->engine = 'InnoDB';
+      $table->engine = 'InnoDB';
 
-			$table->integer('class_id')->unsigned();
-			$table->integer('event_id')->unsigned();
+      $table->increments('id');
 
-			$table->integer('limit');
-			$table->boolean('locked');
+      $table->integer('class_id')->unsigned();
+      $table->integer('event_id')->unsigned();
 
-			$table->foreign('class_id')->references('id')->on('classes');
-			$table->foreign('event_id')->references('id')->on('events');
+      $table->integer('limit');
+      $table->boolean('locked');
 
-		});
-	}
+      $table->foreign('class_id')->references('id')->on('classes');
+      $table->foreign('event_id')->references('id')->on('events');
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('event_classes');
-	}
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('event_classes');
+  }
 
 }
