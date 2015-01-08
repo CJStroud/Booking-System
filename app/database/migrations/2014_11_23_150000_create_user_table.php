0e7790ad321ed7ef6c0f5a4f2c1603b8ceabd5f6
Piefarmer <chris.normansell@gmail.com>
2015-01-08 23:04:17 +0000

    Added banned reason to user table

--- app/database/migrations/2014_11_23_150000_create_user_table.php
+++ app/database/migrations/2014_11_23_150000_create_user_table.php
@@ -5,47 +5,49 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUserTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create( 'users', function( $table ) {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create( 'users', function( $table ) {
 
-			$table->engine = 'InnoDB';
+      $table->engine = 'InnoDB';
 
-			$table->increments('id');
+      $table->increments('id');
 
-			$table->string('forename', 60);
-			$table->string('surname', 60);
-			$table->string('email');
-			$table->string('password');
-			$table->string('secret');
-			$table->string('brca');
-			$table->string('transponder');
+      $table->string('forename', 60);
+      $table->string('surname', 60);
+      $table->string('email');
+      $table->string('password');
+      $table->string('secret');
+      $table->string('brca');
+      $table->string('transponder');
 
-			$table->integer('skill');
+      $table->integer('skill');
 
-			$table->boolean('is_admin');
-			$table->boolean('banned');
+      $table->boolean('is_admin');
 
-			$table->timestamps();
-			$table->softDeletes();
-			$table->rememberToken();
+      $table->integer('banned');
+      $table->boolean('banned_reason');
 
-		});
-	}
+      $table->timestamps();
+      $table->softDeletes();
+      $table->rememberToken();
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('users');
-	}
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 
 }
