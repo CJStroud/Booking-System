ae62575eb882b9bc21cfd6ae5d3c80c6cd712d8e
Piefarmer <chris.normansell@gmail.com>
2015-01-08 17:38:14 +0000

    Added Javascript for sidebar

---
+++ assets/js/SettingsSidebar.js
@@ -0,0 +1,13 @@
+/**
+----------------------------------
+Sidebar moving when in mobile view
+----------------------------------
+**/
+
+$(function Load(){
+  if ($(window).width() < 768) {
+    $('#expand-sidebar').click(function NavigateSidebarClick() {
+
+    });
+  }
+});
