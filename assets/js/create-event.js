0b1e264d539e98aeac5f9e15039dbf8bf8a44bc5
Chris Stroud <chris.stroud00@gmail.com>
2015-01-09 11:43:30 +0000

    limited frequencies that can be selected to 3

--- assets/js/create-event.js
+++ assets/js/create-event.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
-    $(".event-name input").keyup(function() {
-        var value = $(".event-name input").val().replace(/ /g, '-').toLowerCase();
-        $('.event-slug input').val(value);
-    });
+  $(".event-name input").keyup(function() {
+      var value = $(".event-name input").val().replace(/ /g, '-').toLowerCase();
+      $('.event-slug input').val(value);
+  });
+
+  var frequenciesSelected = 0;
+  var maxFrequencies = 3;
+
+  $( ".frequency-option" ).click(function() {
+
+    if ($(this).hasClass('frequency-selected')) {
+      frequenciesSelected--;
+      $(this).toggleClass('frequency-selected');
+    }
+    else if (frequenciesSelected < maxFrequencies) {
+      frequenciesSelected++;
+      $(this).toggleClass('frequency-selected');
+    }
+
+  });
 
 });
