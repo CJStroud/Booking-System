48914f606ff93e6811c274ab7cac0d5df09f738f
stroudiee <chris.stroud00@gmail.com>
2015-01-11 17:43:08 +0000

    turned frequencies into an array

--- app/HMCC/Form/Booking/BookingForm.php
+++ app/HMCC/Form/Booking/BookingForm.php
@@ -6,14 +6,16 @@ use HMCC\Form\Form;
 
 class BookingForm extends Form
 {
-  public function __construct(BookingRepository $repository, BookingValidator $validator)
-  {
-    $this->repository = $repository;
-    $this->validator = $validator;
-  }
+	public function __construct(BookingRepository $repository, BookingValidator $validator)
+	{
+		$this->repository = $repository;
+		$this->validator = $validator;
+	}
 
-  public function store(Array $inputs)
-  {
-    return parent::store($inputs);
-  }
+	public function store(Array $inputs)
+	{
+		$inputs['frequencies'] = explode(",", $inputs['frequency-ids'], 3);
+
+		return parent::store($inputs);
+	}
 }
