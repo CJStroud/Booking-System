4bd5d3d59adbadef798cd78f3d7b6c61ede9cfa6
stroudiee <chris.stroud00@gmail.com>
2015-01-14 20:02:30 +0000

    Basic booking tiles

--- app/views/partials/booking-tile.blade.php
+++ app/views/partials/booking-tile.blade.php
@@ -1,20 +1,34 @@
-<h1>{ $booking->raceEvent }}</h1>
+<div class="col-xs-12">
 
-<h1 class="date">{{ date('d/m/Y H:i', $booking->startTime) }}</h1>
 
-<p>Transponder</p>
+<div class="ticket">
+	<div class="ticket-header">
+			<div class="ticket-event">{{ $booking->raceEvent }}</div>
+			<div class="ticket-date">Starts at <span>{{ date('d/m/Y H:i', $booking->startTime) }}</span>
+		</div>
+	</div>
 
-<p> #{{ $booking->transponder }}</p>
-<p>Skill Level</p>
-<p>{{ $booking->skill_level }}</p>
+	<div class="ticket-body">
 
-<p>Class Name</p>
-<p>{{ $booking->raceClass }}</p>
+		<p>Class {{ $booking->raceClass }}</p>
+		<p>Transponder #{{ $booking->transponder }}</p>
+		<p>Skill Level {{ $booking->skill_level }}</p>
 
-<a class="btn btn-default" href="{{ route('booking.edit', ['id' => $booking->id]) }}">Edit<i class="fa fa-pencil icon-spacing-left"></i></a>
-{{ Form::open(array('route' => ['booking.destroy', $booking->id], 'method' => 'delete')) }}
-<a class="btn btn-default" href="{{ route('booking.destroy', ['id' => $booking->id]) }}">Cancel<i class="fa fa-remove icon-spacing-left"></i></a>
-{{ Form::close() }}
+		<div class="col-xs-12 col-sm-2">
+			<a class="btn btn-default" href="{{ route('booking.edit', ['id' => $booking->id]) }}">Edit<i class="fa fa-pencil icon-spacing-left"></i></a>
+		</div>
+
+		<div class="col-xs-12 col-sm-2">
+			{{ Form::open(array('route' => ['booking.destroy', $booking->id], 'method' => 'delete')) }}
+			<a class="btn btn-default" href="{{ route('booking.destroy', ['id' => $booking->id]) }}">Cancel<i class="fa fa-remove icon-spacing-left"></i></a>
+			{{ Form::close() }}
+		</div>
+
+	</div>
+
+</div>
+
+</div>
 
 
 
