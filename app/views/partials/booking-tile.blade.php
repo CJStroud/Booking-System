d5da0c248ab70511cf316a5c6cd37a2f7105adb8
stroudiee <chris.stroud00@gmail.com>
2015-01-14 21:45:59 +0000

    Initial design for my bookings

--- app/views/partials/booking-tile.blade.php
+++ app/views/partials/booking-tile.blade.php
@@ -1,32 +1,36 @@
-<div class="col-xs-12">
-
-
 <div class="ticket">
 	<div class="ticket-header">
-			<div class="ticket-event">{{ $booking->raceEvent }}</div>
-			<div class="ticket-date">Starts at <span>{{ date('d/m/Y H:i', $booking->startTime) }}</span>
+		<div class="col-sm-6">
+				<div class="ticket-event">{{ $booking->raceEvent }}</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="ticket-event">{{ $booking->raceClass }}</div>
 		</div>
 	</div>
 
 	<div class="ticket-body">
 
-		<p>Class {{ $booking->raceClass }}</p>
-		<p>Transponder #{{ $booking->transponder }}</p>
-		<p>Skill Level {{ $booking->skill_level }}</p>
-
-		<div class="col-xs-12 col-sm-2">
-			<a class="btn btn-default" href="{{ route('booking.edit', ['id' => $booking->id]) }}">Edit<i class="fa fa-pencil icon-spacing-left"></i></a>
+		<div class="col-xs-12">
+			<div class="ticket-date">Start time <span>{{ date('d/m/Y H:i', $booking->startTime) }}</span>
+			</div>
+			<p>Transponder #{{ $booking->transponder }}</p>
+			<p>Skill Level {{ $booking->skill_level }}</p>
 		</div>
 
-		<div class="col-xs-12 col-sm-2">
+
+		<div class="col-xs-6 col-sm-2 col-sm-offset-8">
+			{{ Form::open(array('route' => ['booking.edit', $booking->id], 'method' => 'edit')) }}
+			<button type="submit" class="btn btn-primary btn-with-addon"><span class="btn-text">Change</span><span class="btn-addon btn-addon-primary"><i class="fa fa-pencil"></i></span></button>
+			{{ Form::close() }}
+		</div>
+
+		<div class="col-xs-6 col-sm-2">
 			{{ Form::open(array('route' => ['booking.destroy', $booking->id], 'method' => 'delete')) }}
-			<a class="btn btn-default" href="{{ route('booking.destroy', ['id' => $booking->id]) }}">Cancel<i class="fa fa-remove icon-spacing-left"></i></a>
+			<button type="submit" class="btn btn-primary btn-with-addon"><span class="btn-text">Cancel</span><span class="btn-addon btn-addon-delete"><i class="fa fa-remove"></i></span></button>
 			{{ Form::close() }}
 		</div>
 
 	</div>
-
-</div>
 
 </div>
 
