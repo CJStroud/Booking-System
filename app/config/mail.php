d447bece4119447e3cf021ce4944a7321c3b3427
stroudiee <chris.stroud00@gmail.com>
2015-01-25 23:49:32 +0000

    changed the mail config to send test emails

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.mandrillapp.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'test@mail.com', 'name' => 'testname'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'chris.stroud00@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'MfAtTwxw9HW67-WHYNfy6w',
 
 	/*
 	|--------------------------------------------------------------------------
