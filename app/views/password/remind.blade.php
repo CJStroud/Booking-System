48e7228749028991b15881e62c043add3ea54ace
Chris Stroud <chris.stroud00@gmail.com>
2015-01-26 11:11:08 +0000

    formatted recover password page

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,10 +1,26 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
-	{{ Form::open(array('route' => ['password.send.reminder'])) }}
-		<input type="email" name="email">
-		<input type="submit" value="Send Reminder">
-	{{ Form::close() }}
-</div>
+	<div class="container">
+		<h1>Password Recovery</h1>
+		{{ Form::open(array('route' => ['password.send.reminder'])) }}
+
+
+		<div class="row">
+			<div class='col-xs-12'>
+				<div class="form-group">
+					{{ Form::label('email', 'Email', ['class' => '']) }}
+					<input type="email" class="form-control" name="email" placeholder="example@mail.com">
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-2 col-sm-offset-10">
+				<button type="submit" class="btn btn-primary btn-with-addon"><span class="btn-text">Send Reminder</span><span class="btn-addon btn-addon-primary"><i class="fa fa-arrow-right"></i></span></button>
+			</div>
+		</div>
+
+		{{ Form::close() }}
+	</div>
 @stop
