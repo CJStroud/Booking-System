74b4ea601ce2538801f565602ec238c5b211c13a
CWDN <chris.normansell@gmail.com>
2015-04-22 19:23:50 +0100

    More formattign

--- app/views/admin/classes.blade.php
+++ app/views/admin/classes.blade.php
@@ -2,45 +2,41 @@
 
 @section('settings-content')
 
-  <div class="col-xs-12">
     <div class="col-xs-12">
-      <div class="row">
+    <div class="col-xs-12">
+        <div class="row">
         <div class="col-xs-12 col-sm-9">
-          <h2>Class Management</h2>
+            <h2>Class Management</h2>
         </div>
         <div class="col-xs-12 col-sm-3">
-          <a href="" class="btn btn-info">Create</a>
+            <a href="" class="btn btn-info">Create</a>
         </div>
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
 
-  <div class="col-xs-12">
+    <div class="col-xs-12">
     @foreach ( $classes as $class )
-      <div class="panel panel-default">
+        <div class="panel panel-default">
         <div class="panel-body">
-          <div class="col-xs-12 col-sm-6 col-md-8">
+            <div class="col-xs-12 col-sm-6 col-md-8">
             <h4>{{ $class->name }}</h4>
-          </div>
-          <div class="col-xs-12 col-sm-3 col-md-2">
-            <button type="submit" class="btn btn-primary btn-with-addon">
-              <span class="btn-text">Edit</span>
-              <span class="btn-addon btn-addon-primary">
-                <i class="fa fa-pencil"></i>
-              </span>
-            </button>
-          </div>
-          <div class="col-xs-12 col-sm-3 col-md-2">
-            <button type="submit" class="btn btn-primary btn-with-addon">
-              <span class="btn-text">Remove</span>
-              <span class="btn-addon btn-addon-delete">
-                <i class="fa fa-trash"></i>
-              </span>
-            </button>
-          </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-2">
+                <a href="{{ route('admin.classes.edit', $class->id) }}" type="submit" class="btn btn-simple">
+                    <span class="btn-text">Edit</span>
+                    <i class="fa fa-pencil icon-spacing-left"></i>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-2">
+                <button type="submit" class="btn btn-simple">
+                    <span class="btn-text">Delete</span>
+                    <i class="fa fa-trash icon-spacing-left"></i>
+                </button>
+            </div>
         </div>
-      </div>
+        </div>
     @endforeach
-  </div>
+    </div>
 
 @stop
