958b3d9f62fefac20b002cdcf6153655a98db124
CWDN <chris.normansell@gmail.com>
2015-04-22 19:22:23 +0100

    Bunch of formatting, not really sure what's gone on here

--- app/HMCC/Repository/RaceEvent/RaceEventClassRepository.php
+++ app/HMCC/Repository/RaceEvent/RaceEventClassRepository.php
@@ -4,76 +4,77 @@ use HMCC\Repository\Repository;
 use RaceEventClass;
 use stdClass;
 
-class RaceEventClassRepository extends Repository
-{
-  protected $raceClassRepository;
+class RaceEventClassRepository extends Repository {
 
-  public function __construct(RaceEventClass $raceEventClass, RaceClassRepository $raceClassRepository)
-  {
-    $this->model = $raceEventClass;
-    $this->raceClassRepository = $raceClassRepository;
-  }
+    protected $raceClassRepository;
 
-  public function get($eventId, $classId)
-  {
-    return $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
-  }
-
-  /**
-   * Gets all of the classes for an event
-   * @param   integer $id The event id
-   * @returns array   An array of the classes that are part of the event specified
-   */
-  public function getEventClassesByEventId($id)
-  {
-    $eventClasses = $this->model->where('event_id', '=', $id)->get();
-
-    $array = [];
-
-    foreach($eventClasses as $eventClass)
+    public function __construct(RaceEventClass $raceEventClass, RaceClassRepository $raceClassRepository)
     {
-      $class = $this->raceClassRepository->find($eventClass->class_id);
-
-      $storedClass = new stdClass();
-      $storedClass->id = $class->id;
-      $storedClass->name = $class->name;
-      $storedClass->active = $class->active;
-      $storedClass->locked = $eventClass->locked;
-
-      $storedClass->maxEntrants = $eventClass->limit;
-
-      $array[] = $storedClass;
+        parent::__construct($raceEventClass);
+        $this->raceClassRepository = $raceClassRepository;
     }
 
-    return $array;
+    public function get($eventId, $classId)
+    {
+        return $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
+    }
 
-  }
+    /**
+     * Gets all of the classes for an event
+     * @param   integer $id The event id
+     * @returns array   An array of the classes that are part of the event specified
+     */
+    public function getEventClassesByEventId($id)
+    {
+        $eventClasses = $this->model->where('event_id', '=', $id)->get();
 
-  /**
-   * Locks an event class
-   * @param integer $eventId The id of the event
-   * @param integer $classId The id of the class
-   */
-  public function lock($eventId, $classId)
-  {
-    $eventClass = $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
+        $array = [];
 
-    $eventClass->locked = true;
+        foreach ($eventClasses as $eventClass)
+        {
+            $class = $this->raceClassRepository->find($eventClass->class_id);
 
-    $eventClass->save();
-  }
+            $storedClass = new stdClass();
+            $storedClass->id = $class->id;
+            $storedClass->name = $class->name;
+            $storedClass->active = $class->active;
+            $storedClass->locked = $eventClass->locked;
 
-  /**
-   * Unlocks an event class
-   * @param integer $eventId The id of the event
-   * @param integer $classId The id of the class
-   */
-  public function unlock($eventId, $classId)
-  {
-    $eventClass = $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
+            $storedClass->maxEntrants = $eventClass->limit;
 
-    $eventClass->locked = false;
+            $array[] = $storedClass;
+        }
 
-    $eventClass->save();
-  }
+        return $array;
+
+    }
+
+    /**
+     * Locks an event class
+     * @param integer $eventId The id of the event
+     * @param integer $classId The id of the class
+     */
+    public function lock($eventId, $classId)
+    {
+        $eventClass = $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
+
+        $eventClass->locked = true;
+
+        $eventClass->save();
+    }
+
+    /**
+     * Unlocks an event class
+     * @param integer $eventId The id of the event
+     * @param integer $classId The id of the class
+     */
+    public function unlock($eventId, $classId)
+    {
+        $eventClass = $this->model->where('event_id', '=', $eventId)->where('class_id', '=', $classId)->first();
+        
+        $eventClass->locked = false;
+
+        $eventClass->save();
+    }
+
 }
