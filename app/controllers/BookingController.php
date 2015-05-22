646d1471d7fce9e7991a3f7c1b400640239dce8b
Chris Stroud <chris.stroud00@gmail.com>
2015-05-22 20:44:25 +0100

    Changed indentation from 4 to 2

--- app/controllers/BookingController.php
+++ app/controllers/BookingController.php
@@ -7,78 +7,77 @@ use HMCC\Repository\RaceEvent\RaceEventRepository;
 
 class BookingController extends \BaseController {
 
-    /**
-     * @var BookingForm
-     */
-    protected $form;
+	/**
+	 * @var BookingForm
+	 */
+	protected $form;
 
-    /**
-     * @var RaceEventRepository
-     */
-    protected $raceEventRepository;
+	/**
+	 * @var RaceEventRepository
+	 */
+	protected $raceEventRepository;
 
-    /**
-     * @var FrequencyRepository
-     */
-    protected $frequencyRepository;
+	/**
+	 * @var FrequencyRepository
+	 */
+	protected $frequencyRepository;
 
 
-    /**
-    * @var BookingFrequencyRepository
-    */
-    protected $bookingFrequencyRepository;
+	/**
+	* @var BookingFrequencyRepository
+	*/
+	protected $bookingFrequencyRepository;
 
-    public function __construct(BookingForm $form, RaceEventRepository $raceEventRepository, FrequencyRepository $frequencyRepository, BookingFrequencyRepository $bookingFrequencyRepository)
-    {
-            $this->form = $form;
-            $this->raceEventRepository = $raceEventRepository;
-            $this->frequencyRepository = $frequencyRepository;
-            $this->bookingFrequencyRepository = $bookingFrequencyRepository;
-    }
+	public function __construct(BookingForm $form, RaceEventRepository $raceEventRepository, FrequencyRepository $frequencyRepository, BookingFrequencyRepository $bookingFrequencyRepository)
+	{
+		$this->form = $form;
+		$this->raceEventRepository = $raceEventRepository;
+		$this->frequencyRepository = $frequencyRepository;
+		$this->bookingFrequencyRepository = $bookingFrequencyRepository;
+	}
 
-    public function create($slug) {
-        return $this->createWithClassId($slug, null);
-    }
+	public function create($slug) {
+		return $this->createWithClassId($slug, null);
+	}
 
-    public function createWithClassId($slug, $class_id) {
-        $event = $this->raceEventRepository->getEventBySlug($slug);
-        $classes = $event->classes;
-        $frequencies = $this->frequencyRepository->all();
+	public function createWithClassId($slug, $class_id) {
+		$event = $this->raceEventRepository->getEventBySlug($slug);
+		$classes = $event->classes;
+		$frequencies = $this->frequencyRepository->all();
 
-        return View::make('booking.create')->withEvent($event)->withClasses($classes)->withFrequencies($frequencies)->withSelected($class_id);
-    }
+		return View::make('booking.create')->withEvent($event)->withClasses($classes)->withFrequencies($frequencies)->withSelected($class_id);
+	}
 
-    /**
-     * Saves a new booking record
-     * @returns Redirect redirects the user to the event index page
-     */
-    public function store() {
-        $this->form->store(Input::all());
+	/**
+	 * Saves a new booking record
+	 * @returns Redirect redirects the user to the event index page
+	 */
+	public function store() {
+		$this->form->store(Input::all());
+		return Redirect::route('event.index');
+	}
 
-        return Redirect::route('event.index');
-    }
+	/**
+	* Deletes a booking record using the id given
+	* @param   integer       $id The id of the booking to delete
+	* @returns Redirect.Back redirects the user back to the previous page
+	*/
+	public function destroy($id)
+	{
+		$booking = Booking::find($id);
 
-    /**
-    * Deletes a booking record using the id given
-    * @param   integer       $id The id of the booking to delete
-    * @returns Redirect.Back redirects the user back to the previous page
-    */
-    public function destroy($id)
-    {
-        $booking = Booking::find($id);
+		$this->bookingFrequencyRepository->deleteByBookingId($id);
 
-        $this->bookingFrequencyRepository->deleteByBookingId($id);
+		$booking->delete();
 
-        $booking->delete();
+		return Redirect::back();
+	}
 
-        return Redirect::back();
-    }
-
-    public function showUserBookings() {
-        if (Auth::check()) {
-            $bookings = $this->form->repository->getAllUser(Auth::id());
-            return View::make('booking.showUserBookings')->with('bookings', $bookings);
-        }
-    }
+	public function showUserBookings() {
+		if (Auth::check()) {
+			$bookings = $this->form->repository->getAllUser(Auth::id());
+			return View::make('booking.showUserBookings')->with('bookings', $bookings);
+		}
+	}
 
 }
