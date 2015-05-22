0827dd502f975e3d34e858ca1ddc4077530e88c0
CWDN <chris.normansell@gmail.com>
2015-05-22 17:35:49 +0100

    Added favicon

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,7 +3,8 @@
   <head>
       <title>Halesowen Model Car Club</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
-
+      <link rel="shortcut icon" type="image/png" href="img/favicon.png?v=2">
+      
       {{ HTML::style('css/styles.css') }}
 
   </head>
