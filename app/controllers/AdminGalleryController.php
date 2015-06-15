8610f2fb116e8f82023d8f43d0a1b9df9bb6f2a6
Chris Normansell <cnormansell@localhost.localdomain>
2015-06-15 22:17:50 +0100

    Formatted file

--- app/controllers/AdminGalleryController.php
+++ app/controllers/AdminGalleryController.php
@@ -4,51 +4,51 @@ use HMCC\Repository\Gallery\ImageCollectionRepository;
 use HMCC\Repository\Gallery\ImageRepository;
 use HMCC\Repository\Gallery\UploadRepository;
 
-class AdminGalleryController extends BaseController {
-    
+class AdminGalleryController extends BaseController
+{
     /**
      * @var ImageCollectionRepository
      */
-    protected $imageCollectionRepository;
-    
+    protected $imgCollectionRepo;
+
     /**
      * @var ImageRepository
      */
     protected $imageRepository;
-    
+
     /**
      * @var UploadRepository
      */
     protected $uploadRepository;
-    
+
     public function __construct(
-            ImageCollectionRepository $imageCollectionRepository, 
-            ImageRepository $imageRepository,
-            UploadRepository $uploadRepository)
-    {
-        $this->imageCollectionRepository = $imageCollectionRepository;
+        ImageCollectionRepository $imgCollectionRepo,
+        ImageRepository $imageRepository,
+        UploadRepository $uploadRepository
+    ) {
+        $this->imageCollectionRepository = $imgCollectionRepo;
         $this->imageRepository = $imageRepository;
         $this->uploadRepository = $uploadRepository;
-        
+
     }
-    
+
     public function index()
     {
         $collections = $this->imageCollectionRepository->getAllTopLevelCollections();
         $images = $this->imageRepository->getImagesWithCollections();
-        
+
         $active = 'gallery';
-        
+
         return View::make('admin.gallery.index')
                 ->withActive($active)
                 ->withFolders($collections)
                 ->withImages($images);
     }
-    
+
     public function newFolder()
     {
         $this->imageCollectionRepository->store(Input::all());
-        
+
         return Redirect::back();
     }
 }
