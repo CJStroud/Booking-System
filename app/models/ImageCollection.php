3d0451fbaafda3c221294497dd20c35fb1cd188e
Chris Normansell <chris.normansell@gmail.com>
2015-06-16 18:35:12 +0100

    Added children relationship

--- app/models/ImageCollection.php
+++ app/models/ImageCollection.php
@@ -1,19 +1,23 @@
 <?php
 
 class ImageCollection extends Eloquent {
-    
+
     protected $table = 'image_collections';
     public $timestamps = false;
     protected $fillable = ['name', 'description', 'slug', 'collection_id'];
-    
+
     public function parentCollection()
     {
         return $this->hasOne('ImageCollection', 'id', 'collection_id');
     }
-    
+
     public function images()
     {
         return $this->hasMany('Image');
     }
-    
+
+    public function children()
+    {
+        return $this->hasMany('ImageCollection', 'id', 'collection_id');
+    }
 }
