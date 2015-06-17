816afb4fbfd3d27ba8acb9409ae6fe2c2378ff5b
Chris Normansell <chris.normansell@gmail.com>
2015-06-17 23:19:24 +0100

    Updated gallery index view

--- app/views/admin/gallery/index.blade.php
+++ app/views/admin/gallery/index.blade.php
@@ -4,9 +4,9 @@
 <div class="col-xs-12">
     <h1 class="col-xs-12 col-sm-4 col-md-3">Gallery</h1>
     <div class="col-xs-12 col-sm-4 col-md-2 pull-right">
-        <button type="submit" 
-                class="btn btn-default btn-with-addon" 
-                data-toggle="modal" 
+        <button type="submit"
+                class="btn btn-default btn-with-addon"
+                data-toggle="modal"
                 data-target="#newFolder">
             <span class="btn-text">New Folder</span>
             <span class="btn-addon btn-addon-primary">
@@ -15,9 +15,9 @@
         </button>
     </div>
     <div class="col-xs-12 col-sm-4 col-md-2 pull-right">
-        <button type="submit" 
-                class="btn btn-default btn-with-addon" 
-                data-toggle="modal" 
+        <button type="submit"
+                class="btn btn-default btn-with-addon"
+                data-toggle="modal"
                 data-target="#uploadImage">
             <span class="btn-text">Upload</span>
             <span class="btn-addon btn-addon-primary">
@@ -28,43 +28,63 @@
 </div>
 
 <div class="gallery-admin">
-        
+
     <div class="gallery-folders">
+        @if ($path != '')
+            <a data-name=".."
+                href="<?php
+                    if (strpos($path, '/') > -1) {
+                        $upPath = substr($path, 0, strrpos($path, '/'));
+                    } else {
+                        $upPath = '';
+                    }
+                    echo route('admin.gallery.folder', $upPath) ?>"
+                class="noselect gallery-item gallery-folder"
+                data-id="up"
+                data-placement="bottom"
+                title="..">
+                <h4>
+                    <span class="fa fa-folder fa-2x"></span>
+                        <span class="name">..</span>
+                </h4>
+            </a>
+        @endif
         @foreach($folders as $folder)
-            <div data-name="{{ $folder->name }}" 
-                 class="noselect gallery-item gallery-folder" 
-                 data-id="{{ $folder->id }}" 
-                 data-toggle="tooltip" 
-                 data-placement="bottom" 
-                 title="{{ $folder->name }}">
+            <a data-name="{{ $folder->name }}"
+                href="{{ route('admin.gallery.folder', $folder->path) }}"
+                class="noselect gallery-item gallery-folder gallery-draggable"
+                data-id="{{ $folder->id }}"
+                data-toggle="tooltip"
+                data-placement="bottom"
+                title="{{ $folder->name }}">
                 <h4>
                     <span class="fa fa-folder fa-2x"></span>
                     @if (strlen($folder->name) > 24)
                         <span class="name">{{ substr($folder->name, 0 , 21) . '...' }}</span>
                     @else
-                        <span class="name"">{{ $folder->name }}</span>
-                    @endif                    
+                        <span class="name">{{ $folder->name }}</span>
+                    @endif
                 </h4>
-            </div>
+            </a>
         @endforeach
     </div>
-    
+
     <br>
-    
+
     <div class="gallery-images">
-        
+
         @foreach ($images as $image)
-            <div data-name="{{ $image->name }}" class="media gallery-item gallery-image noselect">
-                <div class="media-left">
-                    <a href="#">
-                        <div class="media-object" style="background-image: url('{{ route('image.inline', $image->upload_id) }}')"></div>
-                    </a>
+            <a href="#" class="gallery-item gallery-image noselect gallery-draggable" data-name="{{ $image->name }}" data-id="{{ $image->id }}">
+                <div class="media">
+                    <div class="media-left">
+                            <div class="media-object" style="background-image: url('{{ route('image.inline', $image->upload_id) }}')"></div>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading name">{{ $image->name }}</h4>
+                        {{ $image->description }}
+                    </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading name">{{ $image->name }}</h4>
-                    {{ $image->description }}
-                </div>
-            </div>
+            </a>
         @endforeach
     </div>
 </div>
@@ -83,6 +103,7 @@
                 <label for="name">Folder name</label>
                 <input type="text" class="form-control" placeholder="Enter folder name" name="name">
             </div>
+            {{ Form::hidden('path', $path) }}
         </div>
         <div class="modal-footer">
           <div class="col-xs-6 col-sm-3 col-sm-offset-6">
@@ -105,7 +126,7 @@
 <div class="modal fade modal-upload-image" id="uploadImage" tabindex="-1" role="dialog" aria-labelledby="Upload Image" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      {{ Form::open(['route' => ['settings.account.delete'], 'role' => 'form', 'id' => 'form', 'method' => 'POST' ] ) }}
+      {{ Form::open(['route' => ['admin.gallery.upload'], 'role' => 'form', 'id' => 'form', 'method' => 'POST', 'files' => true ] ) }}
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <h4 class="modal-title">Upload Image</h4>
@@ -115,17 +136,18 @@
                 <label for="name">Image name</label>
                 <input type="text" class="form-control" id="imageName" placeholder="Enter image name" name="name">
             </div>
-            
+
             <div class="form-group">
                 <label for="description">Image description</label>
                 <textarea class="form-control" placeholder="Enter image description" name="description"></textarea>
             </div>
-            
+
             <div class="form-group">
                 <label for="image-upload">Image upload</label>
                 <input type="file" name="image-upload">
             </div>
-            
+            {{ Form::hidden('path', $path) }}
+
         </div>
         <div class="modal-footer">
           <div class="col-xs-6 col-sm-3 col-sm-offset-6">
@@ -151,16 +173,16 @@
 @section('javascript')
 <script>
     $(function() {
-       $(".gallery-admin .gallery-item").draggable({ 
+       $(".gallery-admin .gallery-draggable").draggable({
             scroll: true,
             scrollSensitivity: 10,
             scrollSpeed: 10,
             stack: ".gallery-admin .gallery-item",
             revert: true,
-            helper: function() {                
+            helper: function() {
                 return $('<div class="gallery-helper"></div>').append($('<span>' + $(this).data('name')  + '</span>').clone());
             },
-            cursor: "default", 
+            cursor: "default",
             cursorAt: { top: -15, left: -5 },
             start: function(event, ui) {
                 $(this).fadeTo("0.3s", 0.5);
@@ -169,21 +191,50 @@
             stop: function(event, ui) {
                 $(this).fadeTo("fast", 1);
                 $(ui.helper).fadeTo("fast", 0);
-            }
+            },
+            distance: 30
         });
-        
+
         $(".gallery-admin .gallery-item img").on('dragstart', function(event) {
             event.preventDefault();
         });
-        
+
         $(".gallery-admin .gallery-folder").droppable({
             tolerance: 'pointer',
             hoverClass: 'gallery-folder-hover',
             drop: function( event, ui ) {
-                $(ui.draggable).fadeTo("0.5s", 0, function() {
-                    $(this).remove();
-                });
                 $(ui.helper).remove();
+
+                var url = '';
+                var data = {};
+                var id = $(ui.draggable).data('id');
+                var newLocation = event.target.href;
+
+                if ($(ui.draggable).hasClass('gallery-image')) {
+                    url = "{{ route('admin.gallery.move-image.ajax') }}";
+                    data = {
+                        newLocation: newLocation,
+                        imageId: id
+                    };
+                } else {
+                    url = "{{ route('admin.gallery.move-folder.ajax') }}";
+                    data = {
+                        newLocation: newLocation,
+                        folderId: id
+                    }
+                }
+
+                $.ajax({
+                    type: 'POST',
+                    url: url,
+                    data: data,
+                    success: function(result) {
+                        $(ui.draggable).fadeTo("0.5s", 0, function() {
+                            $(this).remove();
+                        });
+                    }
+                });
+
             }
         });
     });
