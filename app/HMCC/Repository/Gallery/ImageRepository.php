28226a5167314955f578ba65816467dbd7c5fcd5
Chris Normansell <chris.normansell@gmail.com>
2015-06-17 23:20:48 +0100

    Added traits and renamed getop level function

--- app/HMCC/Repository/Gallery/ImageRepository.php
+++ app/HMCC/Repository/Gallery/ImageRepository.php
@@ -3,8 +3,13 @@ namespace HMCC\Repository\Gallery;
 
 use Image;
 use HMCC\Repository\Repository;
+use HMCC\Repository\Gallery\FindBestSlugTrait;
+use HMCC\Repository\Gallery\BySlugTrait;
 
-class ImageRepository extends Repository {
+class ImageRepository extends Repository
+{
+    use FindBestSlugTrait;
+    use BySlugTrait;
 
     public function __construct(Image $image)
     {
@@ -23,7 +28,7 @@ class ImageRepository extends Repository {
         return rtrim(strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $string))), '-');
     }
 
-    public function getImagesWithCollections()
+    public function getTopLevelImages()
     {
         return $this->model->whereNull('collection_id')->get();
     }
