ada208c8ba966082bbd2453bba4b8af9a04f207b
Chris Normansell <chris.normansell@gmail.com>
2015-06-17 23:13:19 +0100

    Added two traits to help with slugs

---
+++ app/HMCC/Repository/Gallery/FindBestSlugTrait.php
@@ -0,0 +1,28 @@
+<?php
+namespace HMCC\Repository\Gallery;
+
+/**
+ * Finds a unique slug
+ */
+trait FindBestSlugTrait
+{
+    public function getUniqueSlug($slug)
+    {
+        $found = false;
+        $count = 0;
+        $proposedSlug = '';
+        while (!$found) {
+            try {
+                $proposedSlug = $slug;
+                if ($count > 0) {
+                    $proposedSlug .= '-' . $count;
+                }
+                $this->bySlug($proposedSlug);
+                $count++;
+            } catch (\Exception $e) {
+                $found = true;
+            }
+        }
+        return ['slug' => $proposedSlug];
+    }
+}
