f0c073ebc7d41b3b35b671e3123943b75d40353e
Chris Normansell <chris.normansell@gmail.com>
2015-06-17 23:20:22 +0100

    Added functions to help store and move collections

--- app/HMCC/Repository/Gallery/ImageCollectionRepository.php
+++ app/HMCC/Repository/Gallery/ImageCollectionRepository.php
@@ -3,8 +3,13 @@ namespace HMCC\Repository\Gallery;
 
 use ImageCollection;
 use HMCC\Repository\Repository;
+use HMCC\Repository\Gallery\FindBestSlugTrait;
+use HMCC\Repository\Gallery\BySlugTrait;
 
-class ImageCollectionRepository extends Repository {
+class ImageCollectionRepository extends Repository
+{
+    use FindBestSlugTrait;
+    use BySlugTrait;
 
     public function __construct(ImageCollection $imageCollection)
     {
@@ -13,7 +18,19 @@ class ImageCollectionRepository extends Repository {
 
     public function store($data)
     {
+        if ($data['path'] != '') {
+            try {
+                $collection = $this->collectionByPath($data['path']);
+                $data['collection_id'] = $collection->id;
+            } catch (\Exception $e) {
+            }
+        }
         $data['slug'] = $this->toSlug($data['name']);
+
+        $calculated = $this->findSuitableSlug($data['path'], $data['slug']);
+
+        $data['path'] = $calculated['path'];
+        $data['slug'] = $calculated['slug'];
 
         parent::store($data);
     }
@@ -26,5 +43,25 @@ class ImageCollectionRepository extends Repository {
     public function getAllTopLevelCollections()
     {
         return $this->model->whereNull('collection_id')->get();
+    }
+
+    public function collectionByPath($path)
+    {
+        return $this->model->where('path', '=', $path)->firstOrFail();
+    }
+
+    public function allWithPathIncluded($path)
+    {
+        return $this->model->where('path', 'LIKE', $path . '%')->get();
+    }
+
+    public function updateToNewPath($oldPath, $newPath)
+    {
+        $collections = $this->allWithPathIncluded($oldPath);
+
+        foreach ($collections as $collection) {
+            $collection->path = str_replace($oldPath, $newPath, $collection->path);
+            $collection->save();
+        }
     }
 }
