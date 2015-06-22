f958a86b28c2907e9d28a6c2d3fb27df87ac808c
Chris Normansell <chris.normansell@gmail.com>
2015-06-22 22:14:55 +0100

    Removed create event button from events index

--- app/views/event/index.blade.php
+++ app/views/event/index.blade.php
@@ -3,12 +3,7 @@
 @section('content')
 <div class="tiles-container">
     <div class="container">
-        @if (Auth::check() && Auth::user()->is_admin)
-          <div>{{ link_to_route('event.create', 'Create New Event', null, ['class="btn btn-primary create-class"']) }}</div>
-        @endif
-
         <h2>Available to book</h2>
-
         <div class="tiles">
             @foreach($events as $event)
                 @include('partials.event-tile', ['event' => $event])
@@ -17,5 +12,3 @@
     </div>
 </div>
 @stop
-
-
