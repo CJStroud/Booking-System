26e60d942f1efb562c23b0e2cb57e42d821531e4
Chris Stroud <chris.stroud00@gmail.com>
2015-06-23 13:08:08 +0100

    changed download link that is seen on mobile devices

--- app/views/info/rules.blade.php
+++ app/views/info/rules.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   <h1>Club Rules</h1>
   <object data="pdf/club-rules.pdf" type="application/pdf" width=100% height=700px>
-    alt : <a href="pdf/club-rules.pdf">test.pdf</a>
+    Download <a href="pdf/club-rules.pdf">Club Rules</a> pdf file
   </object>
 </div>
 @stop
