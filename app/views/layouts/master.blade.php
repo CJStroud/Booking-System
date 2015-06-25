25525620a57b578a55d4608841716dd926a1f0c1
Chris Normansell <chris.normansell@gmail.com>
2015-06-25 23:09:46 +0100

    Added description to meta tag

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
       <link rel="shortcut icon" type="image/png" href="/img/favicon.png?v=2">
       <meta name="_token" content="{{ csrf_token() }}" />
-      <meta name=”description” content="">
+      <meta name=”description” content="We are Halesowen Model Car Club, a friendly club that races every friday at Hadden Hill Leisure Centre 18.30-22:30pm. Our Attendance is always good and we have various abilities of drivers through out the club. The atmosphere is always very friendly so if you want a good nights racing then come along and join us.">
       <meta name="robots" content="index, follow">
       {{ HTML::style('css/styles.css') }}
 
